43c9bbd100fa820f57d5f90f40d614ffd72b5b05
RifqiMuhA <rifqiahdan03@gmail,com>
2026-03-02 00:54:36 +0700

    fix(types): add meshLine intrinsic types for React 19

--- src/global.d.ts
+++ src/global.d.ts
@@ -8,11 +8,12 @@ declare module 'meshline' {
     export const MeshLineMaterial: any;
 }
 
-declare global {
-    namespace JSX {
-        interface IntrinsicElements {
-            meshLineGeometry: any;
-            meshLineMaterial: any;
-        }
+import { Object3DNode, MaterialNode } from '@react-three/fiber';
+import { MeshLineGeometry, MeshLineMaterial } from 'meshline';
+
+declare module '@react-three/fiber' {
+    interface ThreeElements {
+        meshLineGeometry: Object3DNode<MeshLineGeometry, typeof MeshLineGeometry>;
+        meshLineMaterial: MaterialNode<MeshLineMaterial, typeof MeshLineMaterial>;
     }
 }
